b69ca82608d2b0ba02694912a36d13977414a4ad
Guilherme Silva Andre <guilhermeaandre2018@gmail.com>
2025-04-15 11:23:16 -0300

    ex 5 feito

---
+++ Exercicios/ex5/calcular.php
@@ -0,0 +1,11 @@
+<?php
+$a =0 ;
+$a = rand(0,4);
+$frase = array("Azul: A cor da calma e da tranquilidade, o azul inspira confiança e serenidade, promovendo um ambiente de paz e relaxamento.",
+            "Vermelho: Símbolo da paixão e da energia, o vermelho estimula a ação, a excitação e a vitalidade, podendo também ser associado à agressividade.",
+            "Verde: A cor da natureza e do equilíbrio, o verde transmite calma, harmonia e renovação, conectando com a sensação de bem-estar e crescimento.",
+            "Amarelo: Representando alegria e otimismo, o amarelo estimula a criatividade, a comunicação e a positividade, podendo também ser associado à ansiedade.", 
+            "Roxo: Mistura de calma e energia, o roxo evoca mistério, espiritualidade e criatividade, inspirando introspecção e intuição.");
+
+echo $frase[$a];
+?>
---
+++ Exercicios/ex5/index.php
@@ -0,0 +1,18 @@
+<!DOCTYPE html>
+<html>
+<head>
+    <meta charset="UTF-8">
+    <title>Exercicio 5</title>
+</head>
+<body>
+
+<h2>Exercicio 5</h2>
+
+<h4> 5-F.U.P que o sistema sorteie uma cor aleatória (ex: Azul, Vermelho, Verde, Amarelo, Roxo) e exiba-a para o usuário junto com uma frase característica dessa cor.</h4>
+
+<form action="calcular.php" method="post">
+    <input type="submit" value="Sortear Cor">
+</form>
+
+</body>
+</html>
