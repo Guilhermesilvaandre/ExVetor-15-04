98384d004ed834105114e7d4cc1988a1c73955d3
Guilherme Silva Andre <guilhermeaandre2018@gmail.com>
2025-04-15 09:39:57 -0300

    ex1 iniciADO

---
+++ Exercicios/ex1/index.php
@@ -0,0 +1,18 @@
+<!DOCTYPE html>
+<html>
+<head>
+    <meta charset="UTF-8">
+    <title>Exercicio 1</title>
+</head>
+<body>
+
+<h2>Exercicio 1</h2>
+
+<h4>1-F.U.P que toda vez que o usuário pressionar um botão, gere um número aleatório de 0 a 99 para ele.</h4>
+
+<form action="calcular.php" method="post">
+    <input type="submit" value="Gerar número">
+</form>
+
+</body>
+</html>
